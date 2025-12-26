4c803a9b8f31b9753f06e74a0d83258e0f8229f5
sravantivanjarapu <sravantivanjarapu@gmail.com>
2025-12-26 11:43:28 +0530

    orders in backend

---
+++ orders/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
